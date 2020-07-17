4aaa80e7acb21589b559abb5c74dedb9f7972abe
xolanisighelo <sighelo25@gmail.com>
2020-07-17 11:16:27 +0200

    pip freeze

--- config/settings.py
+++ config/settings.py
@@ -76,7 +76,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'users',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': '123postgres123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
