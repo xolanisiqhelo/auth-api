d694915c4a0e8c146438fc342e31b98a7c152feb
xolanisiqhelo <siqhelo25@gmail.com>
2020-07-21 16:09:12 +0200

    addind reference number to user

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Role(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="role")
     role = models.CharField(max_length=10)
+    referenceNumber = models.CharField(max_length=10)
 
     def __str__(self):
-        return '{},{}'.format(self.user, self.role)
+        return '{},{}'.format(self.user, self.role, self.referenceNumber)
