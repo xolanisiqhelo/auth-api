3d57b2094420b56a5961cf2d2032ff2b770ed7d8
xolanisiqhelo <siqhelo25@gmail.com>
2020-07-26 08:30:14 +0200

    role by id

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import RoleListView, RetrieveUserView, RoleDetailsView
 
 urlpatterns = [
     path('role/', RoleListView.as_view()),
-    path('RoleDetailsView/<pk>', RoleDetailsView.as_view()),
+    path('role/<pk>', RoleDetailsView.as_view()),
     path('role/<referenceNumber>/', RetrieveUserView.as_view())
 
 ]
